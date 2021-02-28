25a136b8ca8f4e1db7d0d69378dbde606c5581b9
sahil keshav <sahilKeshav1997@gmail.com>
2021-02-28 20:21:17 +0530

    deploying to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
-function App() {
+const url = "https://course-api.com/react-tabs-project";
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState(0);
+
+  // console.log(data);
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const finalResponse = await response.json();
+    // console.log(finalResponse);
+    setData(finalResponse);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); //runs only on initial render
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  const { company, dates, duties, title } = data[value]; //first item in data array//destructing
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {data.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)} // set value = index
+                className={`job-btn ${index === value && "active-btn"}`} // if index of current btn === index of current element in data arrary then add the following class to btn .
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((item, i) => {
+            return (
+              <div className="job-desc" key={i}>
+                <FaAngleDoubleRight />
+                <p>{item}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+      <button type="button" className="btn">
+        read more
+      </button>
+    </section>
   );
-}
+};
 
 export default App;
